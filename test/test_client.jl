aa388a9d3c05d2ed9d5cc41dc82aa2173ac6a3c1
Scott Noel-Hemming <frogstarr78@gmail.com>
2015-08-05 20:53:30 -0700

    Re-organizing to test are run more reliably

--- test/test_client.jl
+++ test/test_client.jl
@@ -1,6 +1,9 @@
 using Redis
 
 function test_client_with(cb::Function)
+	if isfile("/tmp/julia-test.rdb")
+		rm("/tmp/julia-test.rdb")
+	end
 	run(`redis-server ${pwd()}/etc/redis.conf`)
 	sleep(1)
 	io = None
@@ -12,12 +15,14 @@ function test_client_with(cb::Function)
 			error(e)
 		finally
 			close(io)
+			open(readall, `redis-cli -p 9999 FLUSHALL`)
 			run(`redis-cli -p 9999 SHUTDOWN`)
 		end
 	catch ee
 		if isopen(io) 
 			close(io)
 		end
+		sleep(5)
 		error(ee)
 	end
 end
